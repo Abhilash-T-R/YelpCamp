8ff2aa30560c4d4663733a72e4fdbf370ff05f9e
Abhilash-T-R <trabhilash1@gmail.com>
2024-10-20 21:19:26 +0530

    "Added author field to Campground model, updated routes and seeds to populate author, and modified show.ejs to display author and restrict edit/delete buttons to author"

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -29,12 +29,13 @@ router.get('/new',isLoggedIn, (req,res)=>{
 })
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req,res,next)=>{
       const c = new Campground(req.body.campground);
+      c.author = req.user._id;
       await c.save();
       req.flash('success','Successfully made a new campground');
       res.redirect(`/campgrounds/${c._id}`);
 }))
 router.get('/:id', catchAsync( async(req,res)=>{
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
     if(!campground){
         req.flash('error','Cannot find that campground!')
         return res.redirect('/campgrounds');
--- seeds/index.js
+++ seeds/index.js
@@ -17,6 +17,7 @@ const seedDB = async ()=>{
         const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
+            author: '6714b627412c76839d0f0f99',
             location: `${cities[random1000].city},${cities[random1000].state}`,
             title:`${sample(descriptors)} ${sample(places)}`,
             image: `https://picsum.photos/400?random=${Math.random()}`,
