12e471a1d2718f599f63d71af5a76e1456ecb9eb
Abhilash-T-R <trabhilash1@gmail.com>
2024-11-15 10:18:09 +0530

    "Updated app.listen to use dynamic port from environment variable"

--- app.js
+++ app.js
@@ -140,6 +140,7 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err});
 })
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
 })
